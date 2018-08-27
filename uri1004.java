44ccce9edbf85b4bebf0da5b2e73bf06e40a8179
Rogério de Souza Oliveira Filho <34068442+oliveirarogerio@users.noreply.github.com>
2018-08-27 00:18:39 -0400

    Add files via upload

---
+++ uri1004.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class uri1004 {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+		int A,B,PROD;
+		
+		A = leia.nextInt();
+		B = leia.nextInt();
+		
+		PROD = A * B;
+		
+		System.out.printf("PROD = %d",PROD);
+		System.out.println();
+
+	}
+
+}
