4aa309c0bfa353dff079baee854709d3ed349ac8
Rogério Oliviera <oliveirarogerio35@gmail.com>
2019-02-13 21:04:25 -0300

    uri

---
+++ java/uri1037.java
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    double n;
+
+    scanf("%lf",&n);
+
+    if(n<0 || n>100)
+
+    {
+
+        printf("Fora de intervalo\n");
+
+    }
+
+    else if(n<=25.00)
+
+    {
+
+        printf("Intervalo [0,25]\n");
+
+    }
+
+    else if(n<=50.0)
+
+    {
+
+        printf("Intervalo (25,50]\n");
+
+    }
+
+    else if(n<=75.0)
+
+    {
+
+        printf("Intervalo (50,75]\n");
+
+    }
+
+    else if(n<=100.0)
+
+    {
+
+        printf("Intervalo (75,100]\n");
+
+    }
+
+ 
+
+    return 0;
+
+}
